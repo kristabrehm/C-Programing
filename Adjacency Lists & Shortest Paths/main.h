927e2bcc30cf9bba753d65b46c8472635e7d22e9
Krista Brehm <111453866+kristabrehm@users.noreply.github.com>
2024-08-19 09:41:56 -0700

    Create main.h

---
+++ Adjacency Lists & Shortest Paths/main.h
@@ -0,0 +1,23 @@
+//main.h file
+
+#ifndef _main_h
+#define _main_h
+
+//inlcude statements
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
+//header files:
+#include "util.h"
+#include "heap.h"
+#include "data_structures.h"
+#include "main.h"
+#include "graph.h"
+
+using namespace std;    //used in main.cpp
+
+
+#endif
